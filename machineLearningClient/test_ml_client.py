8c9c47ece6be11593cd4cf75ba9e4f5ec60fd42f
bensonnli <bensonnzli@gmail.com>
2024-04-30 23:57:58 -0400

    added app2.py test functions

--- machineLearningClient/test_ml_client.py
+++ machineLearningClient/test_ml_client.py
@@ -1,10 +1,13 @@
 import pytest
 import openai
+import os
 from flask import Flask
 from mongomock import MongoClient
 from unittest.mock import patch
 from app2 import create_app, get_key, init_app
 
+# open ai api key for the test_ml_sch_endpoint function
+os.environ["OPENAI_API_KEY"] = "{insert api key here}"
 
 def test_sanity_check():
     expected = True  # the value we expect to be present
@@ -81,3 +84,43 @@ def test_ML_client_fail(db, monkeypatch):
     client = app.test_client()
     response = client.get("/ml_result")
     assert response.status_code == 404
+
+def test_ml_result_endpoint_user_not_found(db, monkeypatch):
+    # No user in the database
+    app = create_app(db, "mock key")
+    app.config["TESTING"] = True
+    client = app.test_client()
+    
+    response = client.get("/ml_result")
+    assert response.status_code == 404
+
+
+def test_ml_search_endpoint(db, monkeypatch):
+    def mock_predict(user_loc, openai_key):
+        return "Mock ML Response"
+    
+    monkeypatch.setattr("app2.predict", mock_predict)
+    
+    app = create_app(db, "mock key")
+    app.config["TESTING"] = True
+    client = app.test_client()
+    
+    response = client.get("/ml_search?loc=New+York")
+    assert response.status_code == 200
+
+def test_ml_sch_endpoint(db, monkeypatch):
+    def mock_predict(user_loc, openai_key):
+        return "Mock ML Response"
+    
+    monkeypatch.setattr("app2.predict", mock_predict)
+    
+    app = create_app(db, "mock key")
+    app.config["TESTING"] = True
+    client = app.test_client()
+    
+    response = client.get("/ml_sch?sch=What+is+the+best+restaurant+in+New+York?")
+    assert response.status_code == 200
+
+
+if __name__ == "__main__":
+    pytest.main()
